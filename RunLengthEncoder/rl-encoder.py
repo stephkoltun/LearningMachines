0df948c20e836076e1dc536d17e1ae3cfae79b10
Stephanie Koltun <stephkoltun@icloud.com>
2017-11-08 19:06:12 -0500

    encoder and random string generator

--- RunLengthEncoder/rl-encoder.py
+++ RunLengthEncoder/rl-encoder.py
@@ -1,13 +1,19 @@
+# encode a string
 def encode (string):
-	print("input string: " + string)
 	code=[]
-	count=1
-	curletter=''
-	prevletter=''
+	count=0			#track sequential instances of the same letter
+	letterIt=1 		#letter iterator - only way I could figure out how to deal with the last character in the string
+	prevletter=''	#letter for comparing through the loop
+
 	for letter in string:
+		#if it's the first letter, assign it as the previous
+		if letterIt == 1:
+			prevletter=letter
+
 		#check if this letter is the same or different than the pevious
 		if letter == prevletter:
-			count++
+			#keep counting
+			count+=1
 		else:
 			# it's different
 			# deal with the previous letter: add count and previous to list
@@ -15,7 +21,33 @@ def encode (string):
 			code.append(pair)
 			# reset count and assign new previous letter
 			count = 1
-			prevletter=letter
+
+		##if it's the last letter, make sure it gets added
+		if letterIt == len(string):
+			pair = (count,letter)
+			code.append(pair)
+
+		prevletter=letter
+		letterIt+=1
+
 	return code
 
-print encode("aaaaahhhhhhmmmmmmmuiiiiiiiaaaaaa")
+# generate random string
+def generateString ():
+	characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','q','r','s','t','u','v','w','x','y','z']
+	genstr = ''
+
+	import random
+
+	for i in range(12):
+		thischar = random.choice(characters)
+		amount = random.randint(1,6)
+		for num in range(amount):
+			genstr += thischar
+	return genstr
+
+
+inputstr = generateString()
+print "original string: " + inputstr 
+print "encoded: "
+print encode(inputstr)
