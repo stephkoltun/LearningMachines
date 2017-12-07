452dc7ff91a025591ed98863efc71c3fa019bd00
Stephanie Koltun <stephkoltun@icloud.com>
2017-12-06 21:50:58 -0500

    working!

--- multi-layer-perceptron/run-mlp.py
+++ multi-layer-perceptron/run-mlp.py
@@ -10,11 +10,13 @@ example_cnt = 150	# number of examples in the dataset
 batch_size  = 10
 epoch_cnt   = 30000
 report_freq = 10
+report_buff = 100
 learn_rate  = 0.05
+
 
 # Construct MLP:
 # takes layer_sizes and activation_fn_name as arguments
-mlp = Mlp( [ sample_size, 15, output_size ], "sig" )
+mlp = Mlp( "iris", [ sample_size, 15, output_size ], "sig" )
 			
 # Construct dataset:
 dataset = constructData("iris.txt", 4)
@@ -22,16 +24,15 @@ dataset = constructData("iris.txt", 4)
 # TO DO split into training and testing (80%, 20%)
 training_inputs = dataset[0]
 training_outputs = dataset[1]
+training_guesses 
 
 print(training_inputs)
 print(training_outputs)
 
 # Train MLP:
-mlp.train( training_inputs, training_outputs, learn_rate, epoch_cnt, batch_size, report_freq )
+# TO DO: Replace with testing inputs (not training twice)
+mlp.train( training_inputs, training_outputs, training_inputs, training_outputs, learn_rate, epoch_cnt, batch_size, report_freq,  report_buff)
 
-# Make predictions:
-# TO DO: Replace with testing inputs (not trainig)
-training_guesses = mlp.predict( training_inputs )
 
 # Print correct and predicted outputs:
 print ( "Outputs: %s\nGuesses: %s\n" ) % ( training_outputs, training_guesses )
